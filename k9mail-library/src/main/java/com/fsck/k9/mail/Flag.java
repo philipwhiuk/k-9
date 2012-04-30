804a37933972ac8552e4abb6eba0133419fdffed
Sander Bogaert <Sander.Bogaert@gmail.com>
2012-04-30 23:36:17 +0200

    Requested fixes. Better variable naming mainly.

--- k9mail-library/src/main/java/com/fsck/k9/mail/Flag.java
+++ k9mail-library/src/main/java/com/fsck/k9/mail/Flag.java
@@ -86,24 +86,25 @@ public class Flag {
      */
     private static final String USER_PREFIX = "USER_";
     
-    private final String name;				// for use towards third party 	ex. "\\Deleted"
-    private final String internal_name;		// for internal use in database,...   ex. "DELETED"
-    protected boolean bCustom;
+    private final String mName;				// for use towards third party 	ex. "\\Deleted"
+    										// when internal name = name we refer to it as just name
+    private final String mInternalName;		// for internal use in database,...   ex. "DELETED"
+    protected boolean mCustom;
     
     /**
      * When a Flag is created dynamically we know it's a custom flag.
      * 
-     * @param internal_name Internal name of the flag.
+     * @param name Internal name of the flag.
      * @return Newly created Flag object.
      */
-    public static Flag createFlag(String internal_name) {
-    	Flag tmpFlag = new Flag(USER_PREFIX+internal_name, internal_name);
-    	tmpFlag.bCustom = true;
+    public static Flag createFlag(String name) {
+    	Flag tmpFlag = new Flag(USER_PREFIX+name, name);
+    	tmpFlag.mCustom = true;
     	return tmpFlag;
 	}
     
-    private Flag(String internal_name){
-    	this(internal_name, internal_name);
+    private Flag(String name){
+    	this(name, name);
     }
     
     /**
@@ -114,9 +115,9 @@ public class Flag {
      * @param name Name for use towards third party ( ex. "\\Deleted" )
      */
     private Flag(String internal_name, String name){
-    	this.name = name;
-    	this.bCustom = false;
-    	this.internal_name = internal_name;
+    	this.mName = name;
+    	this.mCustom = false;
+    	this.mInternalName = internal_name;
     }
     
     /** 
@@ -130,18 +131,18 @@ public class Flag {
      * IMPORTANT remember the name of the field of predefined flags must equal the
      * internal name!
      * 
-     * @param internal_name Name of Flag wanted.
+     * @param name Name of Flag wanted.
      * @return	Predefined Flag object if any otherwise new custom Flag.
      * @throws IllegalArgumentException Thrown when the field is not accessible.
      */
-    public static Flag valueOf(String internal_name) throws IllegalArgumentException{
+    public static Flag valueOf(String name) throws IllegalArgumentException{
     	try {
-			return (Flag)(Flag.class.getField(internal_name).get(null));
+			return (Flag)(Flag.class.getField(name).get(null));
 		} catch (NoSuchFieldException e) {
 			// not a predefined flag
-			if(internal_name.startsWith(USER_PREFIX))
-				return Flag.createFlag(internal_name.substring(USER_PREFIX.length()));
-			else return Flag.createFlag(internal_name);
+			if(name.startsWith(USER_PREFIX))
+				return Flag.createFlag(name.substring(USER_PREFIX.length()));
+			else return Flag.createFlag(name);
 		} catch (IllegalAccessException e) {
 			throw new IllegalArgumentException(e);
 		}
@@ -161,15 +162,15 @@ public class Flag {
      */
     public static Flag valueOfByRealName(String name){
     	for( Flag f : IMAP_FLAGS )
-    		if(f.name.equalsIgnoreCase(name))
+    		if(f.mName.equalsIgnoreCase(name))
     			return f;
     	return Flag.createFlag(name);
     }
     
     @Override
-    public String toString() { return internal_name; }
+    public String toString() { return mInternalName; }
     
-    public String name() { return internal_name; }
+    public String name() { return mInternalName; }
     
     /**
      * Returns the real keyword name without user prefix. This is
@@ -178,6 +179,6 @@ public class Flag {
      * @return Real keyword string.
      */
     public String realName(){
-    	return name;
+    	return mName;
     }
 }
