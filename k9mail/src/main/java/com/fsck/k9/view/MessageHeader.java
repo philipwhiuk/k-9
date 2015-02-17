6dcbe2561a232dacc765ce2b8d437349dd0dc0d2
Igor Almeida <igor.contato@gmail.com>
2015-02-17 19:56:30 -0300

    User keywords: list them in the message view (the ugly way)
    Adds a label+field after "Cc:" listing all user-defined imap keywords in
    the message.

--- k9mail/src/main/java/com/fsck/k9/view/MessageHeader.java
+++ k9mail/src/main/java/com/fsck/k9/view/MessageHeader.java
@@ -49,6 +49,8 @@ public class MessageHeader extends LinearLayout implements OnClickListener, OnLo
     private TextView mToLabel;
     private TextView mCcView;
     private TextView mCcLabel;
+    private TextView mTagsView;
+    private TextView mTagsLabel;
     private TextView mSubjectView;
 
     private View mChip;
@@ -100,6 +102,8 @@ public class MessageHeader extends LinearLayout implements OnClickListener, OnLo
         mToLabel = (TextView) findViewById(R.id.to_label);
         mCcView = (TextView) findViewById(R.id.cc);
         mCcLabel = (TextView) findViewById(R.id.cc_label);
+        mTagsView = (TextView) findViewById(R.id.tags);
+        mTagsLabel = (TextView) findViewById(R.id.tags_label);
 
         mContactBadge = (QuickContactBadge) findViewById(R.id.contact_badge);
 
@@ -119,6 +123,8 @@ public class MessageHeader extends LinearLayout implements OnClickListener, OnLo
         mFontSizes.setViewTextSize(mToLabel, mFontSizes.getMessageViewTo());
         mFontSizes.setViewTextSize(mCcView, mFontSizes.getMessageViewCC());
         mFontSizes.setViewTextSize(mCcLabel, mFontSizes.getMessageViewCC());
+        mFontSizes.setViewTextSize(mTagsView, mFontSizes.getMessageViewCC()); /* same font as cc */
+        mFontSizes.setViewTextSize(mTagsLabel, mFontSizes.getMessageViewCC());
 
         mFromView.setOnClickListener(this);
         mToView.setOnClickListener(this);
@@ -253,6 +259,8 @@ public class MessageHeader extends LinearLayout implements OnClickListener, OnLo
         final CharSequence to = MessageHelper.toFriendly(message.getRecipients(Message.RecipientType.TO), contacts);
         final CharSequence cc = MessageHelper.toFriendly(message.getRecipients(Message.RecipientType.CC), contacts);
 
+        Set<Flag> msgFlags = message.getFlags();
+
         Address[] fromAddrs = message.getFrom();
         Address[] toAddrs = message.getRecipients(Message.RecipientType.TO);
         Address[] ccAddrs = message.getRecipients(Message.RecipientType.CC);
@@ -322,6 +330,21 @@ public class MessageHeader extends LinearLayout implements OnClickListener, OnLo
         mAnsweredIcon.setVisibility(message.isSet(Flag.ANSWERED) ? View.VISIBLE : View.GONE);
         mForwardedIcon.setVisibility(message.isSet(Flag.FORWARDED) ? View.VISIBLE : View.GONE);
         mFlagged.setChecked(message.isSet(Flag.FLAGGED));
+
+        /* Are there user tags? Show them */
+        StringBuilder sb = new StringBuilder();
+        if (!msgFlags.isEmpty()) {
+            for (Flag f : msgFlags) {
+                if (f.isCustom()) {
+                    sb.append(f.realName() + ", ");
+                }
+            }
+
+            if (sb.length() > 0) {
+                sb.setLength(sb.length()-2);
+            }
+        }
+        updateAddressField(mTagsView, sb, mTagsLabel);
 
         mChip.setBackgroundColor(mAccount.getChipColor());
 
