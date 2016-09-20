63de0eb195670865d803b037f256504ed6fb10ee
Philip Whitehouse <philip@whiuk.com>
2016-09-20 23:50:27 +0100

    MockSmtpServer WIP

--- k9mail-library/src/test/java/com/fsck/k9/mail/transport/SmtpTransportTest.java
+++ k9mail-library/src/test/java/com/fsck/k9/mail/transport/SmtpTransportTest.java
@@ -1,15 +1,43 @@
 package com.fsck.k9.mail.transport;
 
 
+import android.annotation.SuppressLint;
+
 import com.fsck.k9.mail.AuthType;
 import com.fsck.k9.mail.ConnectionSecurity;
+import com.fsck.k9.mail.MessagingException;
 import com.fsck.k9.mail.ServerSettings;
+import com.fsck.k9.mail.ssl.TrustedSocketFactory;
+import com.fsck.k9.mail.store.StoreConfig;
+import com.fsck.k9.mail.transport.mockserver.MockSmtpServer;
+
 import org.junit.Test;
 
+import java.io.IOException;
+import java.net.Socket;
+import java.security.KeyManagementException;
+import java.security.NoSuchAlgorithmException;
+import java.security.cert.CertificateException;
+import java.security.cert.X509Certificate;
+
+import javax.net.ssl.SSLContext;
+import javax.net.ssl.SSLSocketFactory;
+import javax.net.ssl.TrustManager;
+import javax.net.ssl.X509TrustManager;
+
 import static org.junit.Assert.assertEquals;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
 
 public class SmtpTransportTest {
+
+    private TrustedSocketFactory trustedSocketFactory = new TestTrustedSocketFactory();
+    private ConnectionSecurity connectionSecurity = ConnectionSecurity.SSL_TLS_REQUIRED;
+    private AuthType authenticationType;
+    private String username;
+    private String password;
+    private String clientCertificateAlias;
 
     @Test
     public void decodeUri_canDecodeAuthType() {
@@ -118,4 +146,92 @@ public class SmtpTransportTest {
 
         assertEquals("smtp://user:password:CRAM_MD5@server:123456", result);
     }
+
+    @Test
+    public void open_canCreateConnectionToTrustedSocket() throws Exception {
+        MockSmtpServer server = new MockSmtpServer();
+        server.output("220 mx1.example.com ESMTP server ready Mon, 01 Jan 2016 01:02:03 +0100");
+        server.expect("HELO client.example.com");
+        SmtpTransport transport = startServerAndCreateSmtpTransport(server);
+
+        transport.open();
+
+        server.verifyConnectionStillOpen();
+        server.verifyInteractionCompleted();
+
+    }
+
+    private SmtpTransport startServerAndCreateSmtpTransport(MockSmtpServer server) throws IOException, MessagingException {
+        server.start();
+        return createSmtpTransport(new ServerSettings(
+                ServerSettings.Type.SMTP, server.getHost(), server.getPort(),
+                connectionSecurity,
+                authenticationType,
+                username,
+                password,
+                clientCertificateAlias), trustedSocketFactory);
+    }
+
+    private SmtpTransport createSmtpTransport(ServerSettings settings, TrustedSocketFactory socketFactory) throws MessagingException {
+        StoreConfig config = mock(StoreConfig.class);
+        when(config.getTransportUri()).thenReturn(SmtpTransport.createUri(settings));
+
+        return new SmtpTransport(config, socketFactory);
+    }
+
+    private SmtpTransport simpleOpen(MockSmtpServer server) throws Exception {
+        return simpleOpenWithCapabilities(server, "");
+    }
+
+    private SmtpTransport simpleOpenWithCapabilities(MockSmtpServer server, String capabilities) throws Exception {
+//        simpleOpenDialog(server, capabilities);
+
+        SmtpTransport smtpTransport = startServerAndCreateSmtpTransport(server);
+        smtpTransport.open();
+
+        return smtpTransport;
+    }
+
+    private static class TestTrustedSocketFactory implements TrustedSocketFactory {
+        @Override
+        public Socket createSocket(Socket socket, String host, int port, String clientCertificateAlias)
+                throws NoSuchAlgorithmException, KeyManagementException, MessagingException, IOException {
+
+            TrustManager[] trustManagers = new TrustManager[] { new VeryTrustingTrustManager() };
+
+            SSLContext sslContext = SSLContext.getInstance("TLS");
+            sslContext.init(null, trustManagers, null);
+
+            SSLSocketFactory sslSocketFactory = sslContext.getSocketFactory();
+
+            if(socket == null) {
+                return sslSocketFactory.createSocket();
+            }
+
+            return sslSocketFactory.createSocket(
+                    socket,
+                    socket.getInetAddress().getHostAddress(),
+                    socket.getPort(),
+                    true);
+        }
+    }
+
+    @SuppressLint("TrustAllX509TrustManager")
+    private static class VeryTrustingTrustManager implements X509TrustManager {
+        @Override
+        public void checkClientTrusted(X509Certificate[] chain, String authType) throws CertificateException {
+            // Accept all certificates
+        }
+
+        @Override
+        public void checkServerTrusted(X509Certificate[] chain, String authType) throws CertificateException {
+            // Accept all certificates
+        }
+
+        @Override
+        public X509Certificate[] getAcceptedIssuers() {
+            return new X509Certificate[0];
+        }
+    }
+
 }
