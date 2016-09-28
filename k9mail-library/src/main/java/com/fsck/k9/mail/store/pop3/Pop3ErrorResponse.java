b2d38fca58a9cd775a5eb7ca5e96f17765f41460
Philip Whitehouse <philip@whiuk.com>
2016-09-28 18:21:26 +0200

    POP3: Extract static classes into separate files

---
+++ k9mail-library/src/main/java/com/fsck/k9/mail/store/pop3/Pop3ErrorResponse.java
@@ -0,0 +1,14 @@
+package com.fsck.k9.mail.store.pop3;
+
+
+import com.fsck.k9.mail.MessagingException;
+
+/**
+ * Exception that is thrown if the server returns an error response.
+ */
+public class Pop3ErrorResponse extends MessagingException {
+    private static final long serialVersionUID = 3672087845857867174L;
+    public Pop3ErrorResponse(String message) {
+        super(message, true);
+    }
+}
