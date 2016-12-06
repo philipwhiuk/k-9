c143a5708842ecd7f5480b9c2fd4031b3989db84
Philip Whitehouse <philip@whiuk.com>
2016-12-06 04:02:32 +0000

    Fix test broken by encoding changes

--- k9mail-library/src/test/java/com/fsck/k9/mail/XOAuth2ChallengeParserTest.java
+++ k9mail-library/src/test/java/com/fsck/k9/mail/XOAuth2ChallengeParserTest.java
@@ -2,14 +2,26 @@ package com.fsck.k9.mail;
 
 import com.fsck.k9.mail.filter.Base64;
 
+import java.io.UnsupportedEncodingException;
+
+import static org.junit.Assert.fail;
+
 
 public class XOAuth2ChallengeParserTest {
-    public static final String STATUS_400_RESPONSE = Base64.encode(
-            "{\"status\":\"400\",\"schemes\":\"bearer mac\",\"scope\":\"https://mail.google.com/\"}");
-    public static final String STATUS_401_RESPONSE = Base64.encode(
-            "{\"status\":\"401\",\"schemes\":\"bearer mac\",\"scope\":\"https://mail.google.com/\"}");
-    public static final String MISSING_STATUS_RESPONSE = Base64.encode(
-            "{\"schemes\":\"bearer mac\",\"scope\":\"https://mail.google.com/\"}");
-    public static final String INVALID_RESPONSE = Base64.encode(
-            "{\"status\":\"401\",\"schemes\":\"bearer mac\",\"scope\":\"https://mail.google.com/\"");
+    public static String STATUS_400_RESPONSE, STATUS_401_RESPONSE, MISSING_STATUS_RESPONSE, INVALID_RESPONSE;
+
+    static {
+        try {
+            STATUS_400_RESPONSE = Base64.encode(
+                    "{\"status\":\"400\",\"schemes\":\"bearer mac\",\"scope\":\"https://mail.google.com/\"}");
+            STATUS_401_RESPONSE = Base64.encode(
+                    "{\"status\":\"401\",\"schemes\":\"bearer mac\",\"scope\":\"https://mail.google.com/\"}");
+            MISSING_STATUS_RESPONSE = Base64.encode(
+                    "{\"schemes\":\"bearer mac\",\"scope\":\"https://mail.google.com/\"}");
+            INVALID_RESPONSE = Base64.encode(
+                    "{\"status\":\"401\",\"schemes\":\"bearer mac\",\"scope\":\"https://mail.google.com/\"");
+        } catch (UnsupportedEncodingException e) {
+            fail();
+        }
+    }
 }
